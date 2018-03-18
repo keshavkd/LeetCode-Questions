95e3115d4edbf88227583b29ea41ae20bb775ac0
keshavkd <keshavkd@users.noreply.github.com>
2018-03-18 12:20:27 +0530

    Added Description

--- HammingDistance.java
+++ HammingDistance.java
@@ -1,3 +1,17 @@
+// The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+
+// Given two integers x and y, calculate the Hamming distance.
+
+// Note:
+// 0 ≤ x, y < 231.
+
+// Example:
+// Input: x = 1, y = 4
+// Output: 2
+// Explanation:
+// 1   (0 0 0 1)
+// 4   (0 1 0 0)
+
 import java.util.*;
 import java.io.*;
 
@@ -34,7 +48,12 @@ class HammingDistance {
         return count;
     }
 
+    public static int hammingD(int x, int y) {
+        return Integer.bitCount(x ^ y);
+    }
+
     public static void main(String[] args) {
         System.out.println("My solution: " + hammingDistance(2,8));
+        System.out.println("Easier solution: " + hammingD(2, 8));
     }
 }
---
+++ JudgeCircle.java
@@ -0,0 +1,52 @@
+// Initially, there is a Robot at position (0, 0). Given a sequence of its moves, 
+// judge if this robot makes a circle, which means it moves back to the original place.
+
+// The move sequence is represented by a string. And each move is represent by a character. 
+// The valid robot moves are R (Right), L (Left), U (Up) and D (down). The output should be true or false representing whether the robot makes a circle.
+
+// Example 1:
+// Input: "UD"
+// Output: true
+
+// Example 2:
+// Input: "LL"
+// Output: false
+
+import java.io.*;
+import java.util.*;
+
+class JudgeCircle {
+
+    public static boolean judgeCircle (String moves) {
+        int x = 0;
+        int y = 0;
+        for (int i = 0; i < moves.length(); i++) {
+            switch (moves.charAt(i)) {
+                case 'U' :
+                    y++;
+                    break;
+                case 'D' :
+                    y--;
+                    break;
+                case 'R' :
+                    x++;
+                    break;
+                case 'L' :
+                    x--;
+                    break;
+                default :
+            }
+        }
+        if (x == 0 && y == 0)
+            return true;
+        else
+            return false;
+    }
+
+    public static void main (String[] args) {
+        String a = "UDDLRU";
+        String b = "LLRUD";
+        System.out.println(judgeCircle(a));
+        System.out.println(judgeCircle(b));
+    }
+}
